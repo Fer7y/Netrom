474fea0fd9a3ea0b23513e959c43ba57716d01c3
Fer7y <a.alecs55@yahoo.ro>
2024-06-26 09:39:39 +0100

    Entity Stage

---
+++ src/Entity/Workout.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkoutRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WorkoutRepository::class)]
+class Workout
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $CreatedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'workouts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $pers = null;
+
+    /**
+     * @var Collection<int, ExerciseLog>
+     */
+    #[ORM\OneToMany(targetEntity: ExerciseLog::class, mappedBy: 'workout', orphanRemoval: true)]
+    private Collection $exerciseLogs;
+
+    public function __construct()
+    {
+        $this->exerciseLogs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->CreatedAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $CreatedAt): static
+    {
+        $this->CreatedAt = $CreatedAt;
+
+        return $this;
+    }
+
+    public function getPers(): ?User
+    {
+        return $this->pers;
+    }
+
+    public function setPers(?User $pers): static
+    {
+        $this->pers = $pers;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ExerciseLog>
+     */
+    public function getExerciseLogs(): Collection
+    {
+        return $this->exerciseLogs;
+    }
+
+    public function addExerciseLog(ExerciseLog $exerciseLog): static
+    {
+        if (!$this->exerciseLogs->contains($exerciseLog)) {
+            $this->exerciseLogs->add($exerciseLog);
+            $exerciseLog->setWorkout($this);
+        }
+
+        return $this;
+    }
+
+    public function removeExerciseLog(ExerciseLog $exerciseLog): static
+    {
+        if ($this->exerciseLogs->removeElement($exerciseLog)) {
+            // set the owning side to null (unless already changed)
+            if ($exerciseLog->getWorkout() === $this) {
+                $exerciseLog->setWorkout(null);
+            }
+        }
+
+        return $this;
+    }
+}
